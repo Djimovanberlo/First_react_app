4dd0394d89f6787d86a40b64647a279e19c2db0d
Djimovanberlo <djimovanberlo@gmail.com>
2020-04-29 09:11:17 +0200

    working template .map callback

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import Title from "./components/Title";
 import Pokemon from "./components/Pokemon";
 import "./App.css";
@@ -9,51 +8,43 @@ const all_pokemon = [
     name: "Raticate",
     weight: 18,
     awesome: true,
-    terrifuing: false,
+    terrifying: false,
     abilities: ["Quick Attack", "Super Fang", "Crunch", "Hyper Fang"],
   },
   {
     name: "Zangooze",
     weight: 40,
     awesome: true,
-    terrifying: false,
+    terrifying: true,
     abilities: ["Slash", "X-Scissor", "Swords Dance", "Brick Break"],
+  },
+  {
+    name: "Jolteon",
+    weight: 25,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Thunder", "Rain Dance", "Thunderbolt", "Thunder Wave"],
   },
 ];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <main>
-          {all_pokemon.map((pokemon) => {
-            const { name, weight, awesome, terrifying, abilities } = pokemon;
-            return (
-              <Pokemon
-                name={name}
-                weight={weight}
-                awesome={awesome}
-                terrifying={terrifying}
-                abilities={abilities}
-              />
-            );
-          })}
-        </main>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Codaisseur
-        </a>
-      </header>
-    </div>
-  );
-}
+    <main className="container">
+      <Title content="Some Simple Title" />
+        <div className="row">
+          {all_pokemon.map(pokemon => (
+              <div className="col-md-6 col-lg-4">
+                <Pokemon
+                  name={pokemon.name}
+                  weight={pokemon.weight}
+                  awesome={pokemon.awesome}
+                  terrifying={pokemon.terrifying}
+                  abilities={pokemon.abilities}
+                />
+              </div>
+          ))}
+        </div>
+      </main>
+  )
 
 export default App;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,16 +1,25 @@
+// src/components/Pokemon.js
 import React from "react";
 
 export default function Pokemon(props) {
   return (
-    <div>
-      <h2>Pokemon name: {props.name}</h2>
-      <p>Weight: {props.weight} kg</p>
-      <p>Awesome: {props.awesome ? "Yes!" : "nope, not really"}</p>
-      <p>Terrifying: {props.terrifying ? "Very" : "nah, lovable"}</p>
-      <p>Abilities: ({props.abilities.length})</p>
-      <ul>
+    <div className="card shadow-sm mb-4">
+      <div class="card-body pb-0">
+        <h5 class="card-title">{props.name}</h5>
+        <h6 class="card-subtitle mb-3 text-primary">
+          {props.awesome ? "An awesome Pokemon" : "Not awesome"}
+        </h6>
+        <p className="mb-0">
+          Weight: {props.weight} kg
+          <br />
+          Terrifying: {props.terrifying ? "Very" : "nah, lovable"}
+          <br />
+          {props.abilities.length} abilities
+        </p>
+      </div>
+      <ul className="list-group list-group-flush">
         {props.abilities.map((ability) => {
-          return <li>{ability}</li>;
+          return <li className="list-group-item">{ability}</li>;
         })}
       </ul>
     </div>
